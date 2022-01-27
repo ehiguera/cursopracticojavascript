0e9975cb5fad93f04038409ecba29b4642d29236
Eduardo Higuera <eduardohiguerag@gmail.com>
2022-01-27 17:45:33 -0600

    comit1je

---
+++ figuras.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5
+console.log("los lados del cuadrado miden "+ladoCuadrado)
+
+const perimetroCuadrado = ladoCuadrado*4
+console.log("perimetro es "+perimetroCuadrado)
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado
+console.log("area es "+areaCuadrado )
